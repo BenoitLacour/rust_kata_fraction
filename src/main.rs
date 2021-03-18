e99d2451ed83e2e0cf0767611cde163c096168e2
fabien.giannesini <fabien.giannesini@gmail.com>
2021-03-18 16:04:55 +0100

    Should add a fraction

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ struct Fraction {
 
 impl Fraction {
     fn add_fraction(&self, fraction: Fraction) -> Fraction {
-        fraction
+        let denominator = fraction.denominator * self.denominator;
+        let numerator = fraction.numerator * self.denominator + self.numerator * fraction.denominator;
+        Fraction {numerator, denominator }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Fraction;
     use std::any::type_name;
+
+    use crate::Fraction;
 
     fn type_of<T>(_: T) -> &'static str {
         type_name::<T>()
@@ -26,5 +29,12 @@ mod tests {
         let fraction = Fraction { numerator: 2, denominator: 3 };
         let result = fraction.add_fraction(Fraction { numerator: 3, denominator: 5 });
         assert_eq!(type_of(result), "fraction_kata::Fraction")
+    }
+
+    #[test]
+    fn should_add_a_fraction() {
+        let fraction = Fraction { numerator: 2, denominator: 3 };
+        let result = fraction.add_fraction(Fraction { numerator: 3, denominator: 5 });
+        assert_eq!(result, Fraction { numerator: 19, denominator: 15 })
     }
 }
